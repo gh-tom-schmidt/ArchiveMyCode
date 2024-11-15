11dab15a69c67f05d1a925b499288194fbe4b75e
Tom Schmidt <92921898+gh-tom-schmidt@users.noreply.github.com>
2024-11-15 19:01:13 +0100

    Delete ParallelVsSerialV2 directory

--- ParallelVsSerialV2/setup-docker.ps1
+++
@@ -1,5 +0,0 @@
-Write-Host "Building the Docker image..."
-docker build -t parallel_vs_serial_v2 .
-
-Write-Host "Running the Docker container..."
-docker run -it --rm parallel_vs_serial_v2 /bin/bash
--- ParallelVsSerialV2/setup-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Building the Docker image..."
-docker build -t parallel_vs_serial_v2 .
-
-echo "Running the Docker container..."
-docker run -it --rm parallel_vs_serial_v2 /bin/bash
-
